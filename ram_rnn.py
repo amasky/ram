642b749d48503dbbc1f36a23f1780313b3b96462
amasky <yamada.masaki.aq@gmail.com>
2016-05-30 11:22:14 +0900

    added gpu support

--- ram_rnn.py
+++ ram_rnn.py
@@ -1,4 +1,5 @@
 import chainer
+from chainer import cuda
 import chainer.functions as F
 import chainer.links as L
 
@@ -9,17 +10,14 @@ _ = 0
 
 class RAM(chainer.Chain):
 
-    # ! not compatible with GPU !
-
     def __init__(self, n_e=128, n_h=256, in_size=28, g_size=8, n_step=6):
         super(RAM, self).__init__(
             emb_l = L.Linear(2, n_e), # l: location
             emb_x = L.Linear(g_size*g_size, n_e), # x: image
             fc_lg = L.Linear(n_e, n_h), # l to g: glimpse
             fc_xg = L.Linear(n_e, n_h), # x to g
-            core_lstm = L.LSTM(n_h, n_h), # core LSTM net
-            #core_hh = L.Linear(n_h, n_h), # core rnn
-            #core_gh = L.Linear(n_e, n_h), # core rnn
+            core_hh = L.Linear(n_h, n_h), # core rnn
+            core_gh = L.Linear(n_h, n_h), # core rnn
             fc_ha = L.Linear(n_h, 10), # h to a: action net
             fc_hl = L.Linear(n_h, 2) # h to l
         )
@@ -29,33 +27,30 @@ class RAM(chainer.Chain):
         self.n_step = n_step
         self.b = None
 
-    def reset_state(self):
-        self.core_lstm.reset_state() # c and h
-
     def clear(self):
         self.loss = None
         self.accuracy = None
 
     def __call__(self, x, t, train=True):
         self.clear()
-        self.reset_state()
 
         # init chainer.Variable
         bs = x.data.shape[0]
+        xp = self.xp
         self.ln_var = chainer.Variable(
-            np.ones(shape=(bs, 1), dtype=np.float32)*np.log(0.01),
+            xp.ones(shape=(bs, 1), dtype=np.float32)*np.log(0.01),
             volatile='auto')
         self.zeros = chainer.Variable(
-            np.zeros(bs, dtype=np.float32), volatile='auto')
+            xp.zeros(bs, dtype=np.float32), volatile='auto')
         self.ones = chainer.Variable(
-            np.ones(bs, dtype=np.float32), volatile='auto')
+            xp.ones(bs, dtype=np.float32), volatile='auto')
 
         # init location and hiddens
         l = chainer.Variable(
-            np.random.uniform(-1, 1, size=(bs,2)).astype(np.float32),
+            xp.random.uniform(-1, 1, size=(bs,2)).astype(np.float32),
             volatile='auto')
         h = chainer.Variable(
-            np.zeros(shape=(bs,self.n_h), dtype=np.float32),
+            xp.zeros(shape=(bs,self.n_h), dtype=np.float32),
             volatile='auto')
 
         # forward n_step times
@@ -70,7 +65,7 @@ class RAM(chainer.Chain):
         # loss with reinforce
         if train:
             conds = chainer.Variable(
-                np.where(np.argmax(y.data,axis=1)==t.data, True, False),
+                xp.where(xp.argmax(y.data,axis=1)==t.data, True, False),
                 volatile='auto')
             r = F.where(conds, self.ones, self.zeros) # reward
             if self.b is None:
@@ -81,7 +76,8 @@ class RAM(chainer.Chain):
 
     def forward(self, h, x, l, train, action):
         # Retina Encoding
-        loc = (l.data+1)*0.5*(self.in_size-self.g_size+1) + self.g_size/2
+        loc = cuda.cupy.asnumpy(l.data)
+        loc = (loc+1)*0.5*(self.in_size-self.g_size+1) + self.g_size/2
         loc = np.clip(loc, self.g_size/2, self.in_size-self.g_size/2)
         loc = np.floor(loc).astype(np.int32)
         hx = crop(x, loc=loc, size=(self.g_size,self.g_size))
@@ -94,8 +90,7 @@ class RAM(chainer.Chain):
         g = F.relu(self.fc_lg(hl) + self.fc_xg(hx))
 
         # Core Net
-        h = self.core_lstm(g) #  LSTM(g + h_t-1)
-        #h = F.relu(self.core_hh(h) + self.core_gh(hx))
+        h = F.relu(self.core_hh(h) + self.core_gh(g))
 
         # Location Net
         l = F.tanh(self.fc_hl(h))
