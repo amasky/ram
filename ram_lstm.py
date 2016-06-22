0bf2821b352f2e3b98882dfa925494a348b1957c
amasky <yamada.masaki.aq@gmail.com>
2016-06-22 19:59:11 +0900

    revised loss with reinforce

--- ram_lstm.py
+++ ram_lstm.py
@@ -25,14 +25,15 @@ class RAM(chainer.Chain):
         self.n_step = n_step
         self.var = 0.03
         self.stddev = 0.173
-        self.b = None
-
-    def reset_state(self):
-        self.core_lstm.reset_state()
+        self.b = 0
 
     def clear(self):
         self.loss = None
         self.accuracy = None
+        self.sum_ln_p = None
+
+    def reset_state(self):
+        self.core_lstm.reset_state()
 
     def __call__(self, x, t, train=True):
         self.clear()
@@ -42,20 +43,30 @@ class RAM(chainer.Chain):
         # init chainer.Variable
         if train:
             self.ln_var = chainer.Variable(
-                self.xp.ones(shape=(bs,2), dtype=np.float32)*np.log(self.var),
-                    volatile=not train)
+                (self.xp.ones(shape=(bs,2), dtype=np.float32)
+                *np.log(self.var)),
+                volatile=not train)
             l = chainer.Variable(
-                self.xp.random.normal(0, self.stddev, size=(bs,2)).astype(np.float32),
+                (self.xp.random.normal(0, self.stddev, size=(bs,2))
+                .astype(np.float32)),
                 volatile=not train)
         else:
             l = chainer.Variable(
                 self.xp.zeros(shape=(bs,2), dtype=np.float32),
                 volatile=not train)
 
-        # forward n_steps
+        # forward n_steps times
         for i in range(self.n_step - 1):
-            l = self.forward(x, l, train, action=False)[0]
-        y, ln_pl = self.forward(x, l, train, action=True)[1:]
+            l, ln_p = self.forward(x, l, train, action=False)[:2]
+            if train:
+                if self.sum_ln_p is None:
+                    self.sum_ln_p = ln_p
+                else:
+                    self.sum_ln_p += ln_p
+
+        ln_p, y = self.forward(x, l, train, action=True)[1:]
+        if train:
+            self.sum_ln_p += ln_p
 
         # loss with softmax cross entropy
         self.loss = F.softmax_cross_entropy(y, t)
@@ -65,22 +76,22 @@ class RAM(chainer.Chain):
         if train:
             r = self.xp.where(
                 self.xp.argmax(y.data,axis=1)==t.data, 1, 0)
-            if self.b is None:
-                self.b = self.xp.sum(r) / bs
             self.b = 0.9*self.b + 0.1*self.xp.sum(r)/bs # bias
-            self.loss += F.sum(ln_pl * (r-self.b)) / bs
+            self.loss += F.sum(self.sum_ln_p * (r-self.b)) / bs
 
         return self.loss
 
     def forward(self, x, l, train, action):
-        # Retina Encoding
         if self.xp == np:
             loc = l.data
         else:
             loc = self.xp.asnumpy(l.data)
-        loc = (loc+1)*0.5*(self.in_size-self.g_size+1) + self.g_size/2
-        loc = np.clip(loc, self.g_size/2, self.in_size-self.g_size/2)
+        margin = self.g_size/2
+        loc = (loc+1)*0.5*(self.in_size-self.g_size+1) + margin
+        loc = np.clip(loc, margin, self.in_size-margin)
         loc = np.floor(loc).astype(np.int32)
+
+        # Retina Encoding
         hx = crop(x, loc=loc, size=self.g_size)
         hx = F.relu(self.emb_x(hx))
 
@@ -100,21 +111,24 @@ class RAM(chainer.Chain):
             # sampling location l
             s = F.gaussian(mean=l, ln_var=self.ln_var)
             s = F.clip(s, -1., 1.)
-        else:
-            s = l
+
+            # location policy
+            l1, l2 = F.split_axis(l, indices_or_sections=2, axis=1)
+            s1, s2 = F.split_axis(s, indices_or_sections=2, axis=1)
+            norm = (s1-l1)*(s1-l1) + (s2-l2)*(s2-l2)
+            ln_p = 0.5 * norm / self.var
+            ln_p = F.reshape(ln_p, (-1,))
 
         if action:
             # Action Net
             y = self.fc_ha(h)
+
             if train:
-                # location policy
-                l1, l2 = F.split_axis(l, indices_or_sections=2, axis=1)
-                s1, s2 = F.split_axis(s, indices_or_sections=2, axis=1)
-                norm = (s1-l1)*(s1-l1) + (s2-l2)*(s2-l2)
-                ln_pl = 0.5 * norm / self.var
-                ln_pl = F.reshape(ln_pl, (-1,))
-                return s, y, ln_pl
+                return s, ln_p, y
             else:
-                return s, y, 0
+                return l, None, y
         else:
-            return s, 0, 0
+            if train:
+                return s, ln_p, None
+            else:
+                return l, None, None
--- ram_wolstm.py
+++ ram_wolstm.py
@@ -26,11 +26,12 @@ class RAM(chainer.Chain):
         self.n_step = n_step
         self.var = 0.03
         self.stddev = 0.173
-        self.b = None
+        self.b = 0
 
     def clear(self):
         self.loss = None
         self.accuracy = None
+        self.sum_ln_p = None
 
     def __call__(self, x, t, train=True):
         self.clear()
@@ -43,20 +44,30 @@ class RAM(chainer.Chain):
 
         if train:
             self.ln_var = chainer.Variable(
-                self.xp.ones(shape=(bs,2), dtype=np.float32)*np.log(self.var),
-                    volatile=not train)
+                (self.xp.ones(shape=(bs,2), dtype=np.float32)
+                *np.log(self.var)),
+                volatile=not train)
             l = chainer.Variable(
-                self.xp.random.normal(0, self.stddev, size=(bs,2)).astype(np.float32),
+                (self.xp.random.normal(0, self.stddev, size=(bs,2))
+                .astype(np.float32)),
                 volatile=not train)
         else:
             l = chainer.Variable(
                 self.xp.zeros(shape=(bs,2), dtype=np.float32),
                 volatile=not train)
 
-        # forward n_steps
+        # forward n_steps times
         for i in range(self.n_step - 1):
-            h, l = self.forward(h, x, l, train, action=False)[:2]
-        y, ln_pl = self.forward(h, x, l, train, action=True)[2:]
+            h, l, ln_p = self.forward(h, x, l, train, action=False)[:3]
+            if train:
+                if self.sum_ln_p is None:
+                    self.sum_ln_p = ln_p
+                else:
+                    self.sum_ln_p += ln_p
+
+        ln_p, y = self.forward(h, x, l, train, action=True)[2:]
+        if train:
+            self.sum_ln_p += ln_p
 
         # loss with softmax cross entropy
         self.loss = F.softmax_cross_entropy(y, t)
@@ -66,22 +77,22 @@ class RAM(chainer.Chain):
         if train:
             r = self.xp.where(
                 self.xp.argmax(y.data,axis=1)==t.data, 1, 0)
-            if self.b is None:
-                self.b = self.xp.sum(r) / bs
             self.b = 0.9*self.b + 0.1*self.xp.sum(r)/bs # bias
-            self.loss += F.sum(ln_pl * (r-self.b)) / bs
+            self.loss += F.sum(self.sum_ln_p * (r-self.b)) / bs
 
         return self.loss
 
     def forward(self, h, x, l, train, action):
-        # Retina Encoding
         if self.xp == np:
             loc = l.data
         else:
             loc = self.xp.asnumpy(l.data)
-        loc = (loc+1)*0.5*(self.in_size-self.g_size+1) + self.g_size/2
-        loc = np.clip(loc, self.g_size/2, self.in_size-self.g_size/2)
+        margin = self.g_size/2
+        loc = (loc+1)*0.5*(self.in_size-self.g_size+1) + margin
+        loc = np.clip(loc, margin, self.in_size-margin)
         loc = np.floor(loc).astype(np.int32)
+
+        # Retina Encoding
         hx = crop(x, loc=loc, size=self.g_size)
         hx = F.relu(self.emb_x(hx))
 
@@ -101,21 +112,24 @@ class RAM(chainer.Chain):
             # sampling location l
             s = F.gaussian(mean=l, ln_var=self.ln_var)
             s = F.clip(s, -1., 1.)
-        else:
-            s = l
+
+            # location policy
+            l1, l2 = F.split_axis(l, indices_or_sections=2, axis=1)
+            s1, s2 = F.split_axis(s, indices_or_sections=2, axis=1)
+            norm = (s1-l1)*(s1-l1) + (s2-l2)*(s2-l2)
+            ln_p = 0.5 * norm / self.var
+            ln_p = F.reshape(ln_p, (-1,))
 
         if action:
             # Action Net
             y = self.fc_ha(h)
+
             if train:
-                # location policy
-                l1, l2 = F.split_axis(l, indices_or_sections=2, axis=1)
-                s1, s2 = F.split_axis(s, indices_or_sections=2, axis=1)
-                norm = (s1-l1)*(s1-l1) + (s2-l2)*(s2-l2)
-                ln_pl = 0.5 * norm / self.var
-                ln_pl = F.reshape(ln_pl, (-1,))
-                return h, s, y, ln_pl
+                return h, s, ln_p, y
             else:
-                return h, s, y, 0
+                return h, l, None, y
         else:
-            return h, s, 0, 0
+            if train:
+                return h, s, ln_p, None
+            else:
+                return h, l, None, None
