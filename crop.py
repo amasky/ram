b16fc5afc0358a324d90ae83433200975738525d
amasky <yamada.masaki.aq@gmail.com>
2016-07-07 11:38:29 +0900

    changed padding in func crop

--- crop.py
+++ crop.py
@@ -6,21 +6,26 @@ class Crop(function.Function):
 
     def __init__(self, loc, size):
         self.size = size
+        self.pad = (size+1) // 2
         self.loc = loc
 
     def forward(self, x):
         xp = cuda.get_array_module(*x)
         n, c, w_i = x[0].shape[:3]
 
-        w_o = self.size
-        m = (w_o+1) // 2
-        loc = (self.loc+1)*0.5*(w_i-w_o+1) + m
-        loc = np.clip(loc, m, w_i-m)
-        loc = np.floor(loc).astype(np.int32)
+        p = self.pad
+        x_p = xp.zeros(shape=(n,c,w_i+2*p,w_i+2*p), dtype=np.float32)
+        x_p[:,:,p:p+w_i,p:p+w_i] = x[0]
 
+        loc = (self.loc+1)*0.5*(w_i+1)
+        loc = np.clip(loc, 0, w_i)
+        loc = np.floor(loc).astype(np.int32)
+        loc += p
+
+        w_o = self.size
         y = xp.zeros(shape=(n,c,w_o,w_o), dtype=np.float32)
         for k in range(n):
-            y[k] = x[0][k,:,loc[k,0]-m:loc[k,0]+m,loc[k,1]-m:loc[k,1]+m]
+            y[k] = x_p[k,:,loc[k,0]-p:loc[k,0]+p,loc[k,1]-p:loc[k,1]+p]
         return y,
 
     # do not backward (always return 0)
