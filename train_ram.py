aad63c7baf12196fa4137c5112cc6a960a1658b4
amasky <yamada.masaki.aq@gmail.com>
2016-10-20 22:34:01 +0900

    speed up of crop func in CPU mode

--- train_ram.py
+++ train_ram.py
@@ -10,28 +10,24 @@ parser.add_argument("-b", "--batchsize", type=int, default=100,
                     help="batch size")
 parser.add_argument("-v", "--variance", type=float, default=0.01,
                     help="variance of the location policy")
-parser.add_argument("-e", "--epoch", type=int, default=2000,
+parser.add_argument("-e", "--epoch", type=int, default=1000,
                     help="iterate training given epoch times")
 parser.add_argument("-f", "--filename", type=str, default="ram",
                     help="prefix of output filenames")
 args = parser.parse_args()
 
-
 import numpy as np
 np.random.seed(777)
-
 from sklearn.datasets import fetch_mldata
 print("preparing dataset...")
 mnist = fetch_mldata("MNIST original")
 mnist.data = mnist.data.astype(np.float32)
 mnist.data = mnist.data.reshape(mnist.data.shape[0], 1, 28, 28)
 mnist.target = mnist.target.astype(np.int32)
-
 train_data, test_data = np.split(mnist.data, [60000], axis=0)
 train_targets, test_targets = np.split(mnist.target, [60000])
 train_data /= 255
 test_data /= 255
-
 
 import chainer
 from chainer import cuda
@@ -46,28 +42,28 @@ else:
     from ram_wolstm import RAM
 model = RAM(n_e=128, n_h=256, g_size=8, n_step=6, scale=1, var=args.variance)
 
-lr_base = 1e-2
+if args.initmodel:
+    print("load model from {}".format(args.initmodel))
+    serializers.load_hdf5(args.initmodel, model)
+
+if not args.lstm:
+    lr_base = 1e-2
+    data = model.core_hh.W.data
+    data[:] = np.identity(data.shape[0], dtype=np.float32)
+else:
+    lr_base = 1e-1
+
 optimizer = chainer.optimizers.NesterovAG(lr=lr_base)
 optimizer.setup(model)
 optimizer.add_hook(chainer.optimizer.GradientClipping(5))
 #optimizer.add_hook(chainer.optimizer.WeightDecay(rate=0.0005))
 model.cleargrads()
 
-if not args.lstm:
-    data = model.core_hh.W.data
-    data[:] = np.identity(data.shape[0], dtype=np.float32)
-
-if args.initmodel:
-    print("load model from {}".format(args.initmodel))
-    serializers.load_hdf5(args.initmodel, model)
-
 gpuid = args.gpu
 xp = cuda.cupy if gpuid >= 0 else np
-
 if gpuid >= 0:
     cuda.get_device(gpuid).use()
     model.to_gpu()
-
 
 import csv
 filename = args.filename
@@ -102,12 +98,11 @@ log.flush()
 sys.stdout.write("test: loss={0:.6f}, accuracy={1:.6f}\n".format(loss, acc))
 sys.stdout.flush()
 
-
 # Learning loop
 batchsize = args.batchsize
 n_data = len(train_targets)
 n_epoch = args.epoch
-lr_gamma = np.exp(-2*np.log(10)/n_epoch) # drop by 10^-2 for n_epoch
+lr_gamma = np.exp(-4*np.log(10)/n_epoch) # drop by 10^-4 for n_epoch
 print("going to train {} epoch".format(n_epoch))
 
 for epoch in range(n_epoch):
