3d320b1257c622dc003b574cba91a05b6f7fce98
amasky <yamada.masaki.aq@gmail.com>
2017-03-31 12:39:52 +0900

    added multi-scale glimpse viz

--- infer.py
+++ infer.py
@@ -1,84 +1,173 @@
 import argparse
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-g", "--gpu", metavar="gpuid", type=int, default=-1,
-                    help="GPU device ID (CPU if negative)")
-parser.add_argument("-m", "--model", metavar="model", type=str,
-                    default="ram.chainermodel", help="chainer model filename")
-parser.add_argument("--lstm", action="store_true",
-                    default=False, help="use LSTM units in core layer")
+group = parser.add_mutually_exclusive_group()
+group.add_argument('--original', action='store_true',
+                    default=True, help='train on original MNIST')
+group.add_argument('--translated', action='store_true',
+                    default=False, help='train on translated MNIST')
+group.add_argument('--cluttered', action='store_true',
+                    default=False, help='train on translated & cluttered MNIST')
+parser.add_argument('--lstm', type=bool, default=True,
+                    help='use LSTM units in core layer')
+parser.add_argument('-m', '--initmodel', type=str,
+                    default='ram_original.chainermodel',
+                    help='load model from given file')
+parser.add_argument('-g', '--gpuid', type=int, default=-1,
+                    help='GPU device ID (CPU if negative)')
 args = parser.parse_args()
 
+
 import numpy as np
-from sklearn.datasets import fetch_mldata
-print("preparing MNIST dataset...")
-mnist = fetch_mldata("MNIST original")
-mnist.data = mnist.data.astype(np.float32)
-mnist.data = mnist.data.reshape(mnist.data.shape[0], 1, 28, 28)
-mnist.target = mnist.target.astype(np.int32)
-
-train_data, test_data = np.split(mnist.data, [60000], axis=0)
-train_targets, test_targets = np.split(mnist.target, [60000])
-train_data /= 255
-test_data /= 255
-
+#np.random.seed(777)
 import chainer
 from chainer import cuda
-import chainer.functions as F
-import chainer.links as L
 from chainer import serializers
+import chainer.functions as F
 
-if args.lstm:
-    from ram_lstm import RAM
-else:
-    from ram_wolstm import RAM
-g_size = 8
-n_step = 6
-model = RAM(n_e=128, n_h=256, g_size=g_size, n_step=n_step)
-serializers.load_hdf5(args.model, model)
+train, test = chainer.datasets.get_mnist()
+train_data, train_targets = np.array(train).transpose()
+test_data, test_targets = np.array(test).transpose()
+train_data = np.array(list(train_data)).reshape(train_data.shape[0],1,28,28)
+test_data = np.array(list(test_data)).reshape(test_data.shape[0],1,28,28)
+train_targets = np.array(train_targets).astype(np.int32)
+test_targets = np.array(test_targets).astype(np.int32)
 
-gpuid = args.gpu
+
+if args.original:
+    filename = 'ram_original'
+    # RAM params for original MNIST
+    g_size = 8
+    n_step = 6
+    scale = 1
+
+    def process(batch):
+        return batch.copy()
+
+if args.translated:
+    filename = 'ram_translated'
+    g_size = 12
+    n_step = 8
+    scale = 3
+
+    # create translated MNIST
+    def translate(batch):
+        n, c, w_i = batch.shape[:3]
+        w_o = 60
+        data = np.zeros(shape=(n,c,w_o,w_o), dtype=np.float32)
+        for k in range(n):
+            i, j = np.random.randint(0, w_o-w_i, size=2)
+            data[k, :, i:i+w_i, j:j+w_i] = batch[k].copy()
+        return data
+
+    process = translate
+
+if args.cluttered:
+    filename = 'ram_cluttered'
+    g_size = 12
+    n_step = 8
+    scale = 3
+
+    # create cluttered MNIST
+    def clutter(batch):
+        n, c, w_i = batch.shape[:3]
+        w_o = 60
+        data = np.zeros(shape=(n,c,w_o,w_o), dtype=np.float32)
+        for k in range(n):
+            i, j = np.random.randint(0, w_o-w_i, size=2)
+            data[k, :, i:i+w_i, j:j+w_i] = batch[k].copy()
+            for _ in range(4):
+                clt = train_data[np.random.randint(0, train_data.shape[0]-1)]
+                c1, c2 = np.random.randint(0, w_i-8, size=2)
+                i1, i2 = np.random.randint(0, w_o-8, size=2)
+                data[k, :, i1:i1+8, i2:i2+8] += clt[:, c1:c1+8, c2:c2+8]
+        data = np.clip(data, 0., 1.)
+        return data
+
+    process = clutter
+
+
+from ram import RAM
+model = RAM(n_e=128, n_h=256, g_size=g_size, n_step=n_step,
+            scale=scale, use_lstm=args.lstm)
+
+print('load model from {}'.format(args.initmodel))
+serializers.load_hdf5(args.initmodel, model)
+
+gpuid = args.gpuid
 xp = cuda.cupy if gpuid >= 0 else np
 if gpuid >= 0:
     cuda.get_device(gpuid).use()
     model.to_gpu()
 
+
 # inference
-index = np.random.randint(0, 9999)
-x = chainer.Variable(
-    xp.asarray(test_data[index:index+1].copy()),
-    volatile="on")
-y, centers = model.infer(x, init_loc=np.random.uniform(-1,1,size=2))
-
-# green if correct otherwise red
-if y == test_targets[index]:
-    color = (0, 255, 0)
-else:
-    color = (255, 0, 0)
-
-# loc in real values to index
-in_size = np.asarray(test_data.shape[2:4])
-centers = 0.5 * (centers+1) * (in_size+1)
-toplefts = centers - 0.5*g_size
-toplefts = np.round(toplefts).astype(np.int32)
-
-# plot results
 import matplotlib.pyplot as plt
-plt.style.use("ggplot")
-plt.figure(figsize=(8, 1.5))
-
+plt.style.use('ggplot')
 import PIL
 from PIL import ImageDraw
-image = PIL.Image.fromarray(test_data[index][0]*255).convert('RGB')
 
-for i in range(n_step):
-    plt.subplot(1, n_step, i+1)
-    img_i = image.copy()
-    draw = ImageDraw.Draw(img_i)
-    xy=[toplefts[i,1],toplefts[i,0],toplefts[i,1]+g_size,toplefts[i,0]+g_size]
-    draw.rectangle(xy=xy, outline=color)
+test_data = process(test_data)
+index = np.random.randint(0, 9999)
+image = PIL.Image.fromarray(test_data[index][0]*255).convert('RGB')
+x = chainer.Variable(
+    xp.asarray(test_data[index][np.newaxis,:,:,:]), volatile='on')
+init_l = np.random.uniform(low=-1, high=1, size=2)
+y, ys, ls = model.infer(x, init_l)
+locs = ((ls+1) / 2) * (np.array(test_data.shape[2:4])+1)
+
+
+# plot results
+from crop import crop
+plt.subplots_adjust(wspace=0.1, hspace=0.1)
+
+for t in range(0, n_step):
+    # digit with glimpse
+    plt.subplot(3+scale, n_step, t+1)
+
+    # green if correct otherwise red
+    if np.argmax(ys[t]) == test_targets[index]:
+        color = (0, 255, 0)
+    else:
+        color = (255, 0, 0)
+
+    canvas = image.copy()
+    draw = ImageDraw.Draw(canvas)
+    xy = np.array([locs[t,1],locs[t,0],locs[t,1],locs[t,0]])
+    wh = np.array([-g_size//2, -g_size//2, g_size//2, g_size//2])
+    xys = [xy + np.power(2,s)*wh for s in range(scale)]
+
+    for xy in xys:
+        draw.rectangle(xy=list(xy), outline=color)
     del draw
-    plt.imshow(img_i, interpolation="none")
-    plt.axis("off")
-    plt.title("t="+str(i+1))
+    plt.imshow(canvas)
+    plt.axis('off')
+
+    # glimpse at each scale
+    gs = crop(x, center=ls[t:t+1], size=g_size)
+    plt.subplot(3+scale, n_step, n_step + t+1)
+    plt.imshow(gs.data[0,0], cmap='gray')
+    plt.axis('off')
+
+    for k in range(1, scale):
+        s = np.power(2,k)
+        patch = crop(x, center=ls[t:t+1], size=g_size*s)
+        patch = F.average_pooling_2d(patch, ksize=s)
+        gs = F.concat((gs, patch), axis=1)
+        plt.subplot(3+scale, n_step, n_step*(k+1) + t+1)
+        plt.imshow(gs.data[0,k], cmap='gray')
+        plt.axis('off')
+
+    # output probability
+    plt.subplot2grid((3+scale,n_step), (1+scale,t), rowspan=2)
+    plt.barh(np.arange(10), ys[t], align='center')
+    plt.xlim(0, 1)
+    plt.ylim(-0.5, 9.5)
+
+    if t == 0:
+        plt.yticks(np.arange(10))
+    else:
+        plt.yticks(np.arange(10), ['' for _ in range(10)])
+    plt.xticks([])
 
 plt.show()
