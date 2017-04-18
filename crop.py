153f458f08f72cf76c1e3acc4ac1c71fe839f552
amasky <yamada.masaki.aq@gmail.com>
2017-04-18 20:18:06 +0900

    made crop function faster using min/max

--- crop.py
+++ crop.py
@@ -15,28 +15,29 @@ class Crop(function.Function):
         xp = cuda.get_array_module(*x)
         n, c, h_i, w_i = x[0].shape
         size_i = np.asarray(x[0].shape[2:4])
-        # [-1, 1]^2 -> [0, h_i]x[0, w_i]
-        center = 0.5 * (self.center+1) * (size_i+1)
-        # tl: topleft
-        tl = center - 0.5*self.size
-        tl = np.round(tl).astype(np.int32)
-
-        h_o, w_o = self.size
+        size_o = self.size
+        h_o, w_o = size_o
         y = xp.zeros(shape=(n,c,h_o,w_o), dtype=np.float32)
 
+        # [-1, 1]^2 -> [0, h_i]x[0, w_i]
+        center = 0.5 * (self.center+1) * (size_i+1)
+
+        # topleft: np.array[batch, [top, left]]
+        topleft = center - 0.5*size_o
+        topleft = np.round(topleft).astype(np.int32)
+
+        tl_o = np.maximum(topleft, 0)
+        br_o = np.minimum(topleft+size_o, size_i)
+
+        tl_i = tl_o - topleft
+        br_i = br_o - topleft
+
         for k in range(n):
-            tl_y, tl_x = tl[k] # k-th batch
-            range_y = np.arange(tl_y, tl_y+h_o)
-            range_x = np.arange(tl_x, tl_x+w_o)
-            cndt_y = (range_y < h_i) & (range_y > -1)
-            cndt_x = (range_x < w_i) & (range_x > -1)
-            idx_y = range_y[cndt_y]
-            idx_x = range_x[cndt_x]
-            cndt_y = np.where(cndt_y)[0]
-            cndt_x = np.where(cndt_x)[0]
-            if not cndt_y.size or not cndt_x.size: continue
-            y[k,:,cndt_y[0]:cndt_y[-1]+1,cndt_x[0]:cndt_x[-1]+1] \
-                += x[0][k,:,idx_y[0]:idx_y[-1]+1,idx_x[0]:idx_x[-1]+1]
+            if (br_i[k,0] - tl_i[k,0]) < 0 or (br_i[k,1] - tl_i[k,1]) < 0:
+                continue
+            y[k,:,tl_i[k,0]:br_i[k,0],tl_i[k,1]:br_i[k,1]] \
+                += x[0][k,:,tl_o[k,0]:br_o[k,0],tl_o[k,1]:br_o[k,1]]
+
         return y,
 
     # do not backward (always return 0)
